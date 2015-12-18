2a415cecb0603432299e459734bfaa089ef8fc3e
Chris Stevenson <chris.j.stevenson@gmail.com>
2015-12-18 15:36:38 -0600

    adding test for create

--- tests/route_test.go
+++ tests/route_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"github.com/chrisjstevenson/go-gin-mgo/handlers/things"
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -13,7 +14,21 @@ func TestList(t *testing.T) {
 	router := gin.New()
 	router.GET("/testList", things.List)
 
-	req, _ := http.NewRequest("GET", "/foo", nil)
+	req, _ := http.NewRequest("GET", "/stuff", nil)
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	assert.NotNil(t, resp.Body.String())
+}
+
+func TestCreate(t *testing.T) {
+	router := gin.New()
+	router.GET("/testCreate", things.Create)
+
+	json := []byte(`{"animal":"furby"}`)
+
+
+	req, _ := http.NewRequest("POST", "/stuff", bytes.NewBuffer(json))
 	resp := httptest.NewRecorder()
 	router.ServeHTTP(resp, req)
 
